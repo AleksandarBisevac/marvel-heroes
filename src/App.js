04df1e128f9149631baee10fe2d73486d85aea6d
AleksandarBisevac <alek.bisevac@gmail.com>
2021-09-04 14:11:41 +0200

    file structure change, problem with HomePage = Homepage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Fragment } from "react";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import HomePage from "./Main/Homepage/HomePage";
 import SingleCharacter from "./Main/SingleCharacter/SingleCharacter";
+import Homepage from "./Main/Homepage/HomePage";
 
 function App() {
   return (
     <Fragment>
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={Homepage} />
         <Route path="/singe-hero/:id" component={SingleCharacter} />
       </Switch>
       <Footer />
--- src/Main/Homepage/Homepage.jsx
+++ src/Main/Homepage/Homepage.jsx
@@ -3,21 +3,22 @@ import SearchBar from "./SearchBar/SearchBar";
 import MyTeam from "./UI/MyTeam/MyTeam";
 import SomeHero from "./UI/SomeHero/SomeHero";
 
+import classes from "./Homepage.module.css";
 
-import classes from "./HomePage.module.css";
-
-const HomePage = () => {
-  return <div className={classes["homepage-wrapper"]}>
-    <SearchBar />
-    <main className={classes.main}>
-      <section className={classes["some-hero"]}>
-        <SomeHero />
-      </section>
-      <aside>
-        <MyTeam />
-      </aside>
-    </main>
-  </div>;
+const Homepage = () => {
+  return (
+    <div className={classes["homepage-wrapper"]}>
+      <SearchBar />
+      <main className={classes.main}>
+        <section className={classes["some-hero"]}>
+          <SomeHero />
+        </section>
+        <aside>
+          <MyTeam />
+        </aside>
+      </main>
+    </div>
+  );
 };
 
-export default HomePage;
+export default Homepage;
